52a4bf9dabac66eee7aa42e96cc82d767f15a184
welove71 <welove71@gmail.com>
2023-03-26 00:41:22 +0900

    2

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
-from random import *
+print("나는 %d살입니다." %20)
+print("나는 %s을 좋아해요." %"파이썬")
 
-print(random())
+
+
+# jumin = "990120-1234567"
+
+
+# print("성별 : " + jumin[7])
+# print("년 :" + jumin[0:2])
+# print("월 :" + jumin[2:4])
+# print("월 :" + jumin[2:4])
+# print("일 :" + jumin[4:6])
+
+# print("생년월일 :" + jumin[:6])
+# print("뒤 7자리 :" + jumin[7:])
